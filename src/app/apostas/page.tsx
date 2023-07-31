70ff22233eb50f43fa7b313257ad7168f47b9158
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-31 15:33:35 -0300

    tabela iniciada teste

--- src/app/apostas/page.tsx
+++ src/app/apostas/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AppBar from '../components/appBar'
-import TabelaApostas from '../components/tabelaApostas'
 import TabelaSimples from '../components/tabelaSimples'
 
 export default function Apostas() {
